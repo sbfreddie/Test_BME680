9d87e7684ce30554fc2c1c46fd33a5f194e0a3c6
sbfreddie <freddie65@comcast.net>
2022-01-01 17:42:04 +0100

    Initial upload of files

---
+++ include/Version.h
@@ -0,0 +1,9 @@
+
+    // AUTO GENERATED FILE, DO NOT EDIT
+    #ifndef VERSION
+        #define VERSION "1.0.0"
+    #endif
+    #ifndef BUILD_TIMESTAMP
+        #define BUILD_TIMESTAMP "2021-12-29 15:57:44.703897"
+    #endif
+    
